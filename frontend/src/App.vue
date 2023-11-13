54adf344a0262db5c7933bfa916323d45e2793c9
zhanghan2021 <zhanghan@kylinos.cn>
2023-11-13 17:34:25 +0800

    Set language to zhCn

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <RouterView />
+  <el-config-provider :locale="zhCn">
+    <RouterView />
+  </el-config-provider>
 </template>
 
-<style scoped lang="scss">
-</style>
+<script setup lang="ts">
+import { ref } from 'vue'
+import locale from 'element-plus/es/locale/lang/zh-cn';
+const zhCn = ref(locale); 
+</script>
+
+<style scoped lang="scss"></style>
