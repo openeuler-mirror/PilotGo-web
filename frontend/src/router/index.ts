a735fa0de21d8756ebad7cf472422098704be691
zhanghan2021 <zhanghan@kylinos.cn>
2023-11-14 10:07:41 +0800

    Add User Management Table Header

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,23 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      redirect:"/home",
+      redirect: '/home',
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import("../views/Login/Login.vue")
+      component: () => import('@/views/Login/Login.vue'),
     },
     {
       path: '/home',
       name: 'home',
-      component: () => import("../views/Home/Home.vue")
+      component: () => import('@/views/Home/Home.vue'),
     },
-  ]
-})
+    {
+      path: '/usermanager',
+      name: 'UserManager',
+      component: () => import('@/views/UserManager/UserManage.vue'),
+      meta: {
+        title: 'usermanager',
+        header_title: '用户管理',
+        panel: 'usermanager',
+        icon_class: 'el-icon-user-solid',
+        breadcrumb: [{ name: '用户管理' }],
+      },
+    },
+  ],
+});
 
-export default router
+export default router;
