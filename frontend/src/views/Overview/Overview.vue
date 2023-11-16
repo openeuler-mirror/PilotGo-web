a3e124c7a2a537b2984fac2e6e2d3e30aa18b99c
Gzx1999 <guozhengxin@kylinos.cn>
2023-11-16 15:09:01 +0800

    add overview page total panel layout

--- frontend/src/views/Overview/Overview.vue
+++ frontend/src/views/Overview/Overview.vue
@@ -1,15 +1,16 @@
 <template>
     <div class="content">
         <div class="total">
-            <div>
-                <div class="curr">
+            <div class="top">
+                <div class="user">
                     <span>欢迎您：{{ userName }}</span>
                     <span>所属部门：{{ userDeptName }}</span>
                     <span>您的身份：{{ userType }}</span>
                 </div>
-                <div>
-                    <el-carousel trigger="click" :interval="8000" indicator-position="none">
-                        <el-carousel-item v-for="item in tooltips" :key="item.id">
+                <div class="tips">
+                    <el-carousel style="width: 100%; height: 100%;" trigger="click" :interval="8000"
+                        indicator-position="none">
+                        <el-carousel-item class="tips_item" v-for="item in tooltips" :key="item.id">
                             <h4 class="small">{{ item.name }}</h4>
                             <p>{{ item.description }}</p>
                         </el-carousel-item>
@@ -52,22 +53,22 @@ const offline = ref(0);
 const free = ref(0);
 
 let tooltips = ref([
-  {
-    id: 1,
-    name: "系统",
-    description:
-      "按部门树节点查看一台机器各指标详情，监控性能变化，创建批次方便批量操作",
-  },
-  {
-    id: 2,
-    name: "批次",
-    description: "查看创建批次，执行下发rpm包动作",
-  },
-  {
-    id: 3,
-    name: "日志",
-    description: "查看执行动作的结果以及失败的具体原因",
-  },
+    {
+        id: 1,
+        name: "系统",
+        description:
+            "按部门树节点查看一台机器各指标详情，监控性能变化，创建批次方便批量操作",
+    },
+    {
+        id: 2,
+        name: "批次",
+        description: "查看创建批次，执行下发rpm包动作",
+    },
+    {
+        id: 3,
+        name: "日志",
+        description: "查看执行动作的结果以及失败的具体原因",
+    },
 ]);
 
 </script>
@@ -81,8 +82,96 @@ let tooltips = ref([
 
     .total {
         width: 48%;
-        height: 45%;
+        height: 40%;
+        box-shadow: 0 6px 12px 0 rgba(0, 0, 0, 0.1);
 
+        .top {
+            width: 96%;
+            height: 50%;
+            margin: 0 auto;
+            display: flex;
+            justify-content: space-evenly;
+            align-items: center;
+
+            .user {
+                width: 46%;
+                height: 100%;
+                border-radius: 3%;
+                border: 1px dashed #aaa;
+                color: rgb(106, 106, 106);
+
+                display: flex;
+                flex-direction: column;
+                justify-content: space-evenly;
+                align-items: center;
+
+                span {
+                    display: inline-block;
+                    width: 80%;
+                }
+            }
+
+            .tips {
+                width: 48%;
+                height: 100%;
+                border-radius: 3%;
+                border: 1px dashed #aaa;
+                color: rgb(106, 106, 106);
+                display: flex;
+                padding-top: 2%;
+
+                :deep(.el-carousel__container) {
+                    height: 100%;
+                }
+
+                .tips_item {
+                    height: 50%
+                }
+
+                h4 {
+                    padding: 0 0 0 2%;
+                }
+
+                p {
+                    font-size: 14px;
+                    padding: 2% 0 0 2%;
+                }
+            }
+        }
+
+        .bottom {
+            width: 100%;
+            height: 50%;
+            display: flex;
+            justify-content: space-evenly;
+            align-items: center;
+
+            .top_panel {
+                width: 28%;
+                height: 64%;
+                border: 1px dashed #aaa;
+                border-radius: 6%;
+                display: flex;
+                flex-direction: column;
+                justify-content: space-evenly;
+                align-items: flex-end;
+                color: rgb(106, 106, 106);
+
+                span {
+                    display: inline-block;
+                    width: 68%;
+                    font-size: 16px;
+                    text-align: center;
+                }
+
+                p {
+                    width: 68%;
+                    text-align: center;
+                    font-size: 24px;
+                    font-weight: bold;
+                }
+            }
+        }
     }
 
     .recent {
