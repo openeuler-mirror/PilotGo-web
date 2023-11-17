b4e98c92e3d7fa46b5fae2cf8d8184402dd87d99
Gzx1999 <guozhengxin@kylinos.cn>
2023-11-17 17:33:39 +0800

    add PGTable framework

--- frontend/src/components/PGTable.vue
+++ frontend/src/components/PGTable.vue
@@ -1,8 +1,41 @@
 <template>
-    <p>PilotGo Table</p>
+    <div class="container">
+        <div class="header">
+            <p>table header</p>
+        </div>
+        <div class="content">
+            <p>table content</p>
+        </div>
+        <div class="pagination">
+            <p>pagination content</p>
+        </div>
+    </div>
 </template>
 
 <script lang="ts" setup>
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+
+    .header {
+        width: 100%;
+        height: 200px;
+    }
+
+    .content {
+        width: 100%;
+        height: 100%;
+    }
+
+    .pagination {
+        width: 100%;
+        height: 40px;
+
+    }
+}
+</style>
