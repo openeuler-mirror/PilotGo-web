6d69535b2216530a78f0893d7844bb9923f20814
Gzx1999 <guozhengxin@kylinos.cn>
2023-11-20 10:52:44 +0800

    add PGTree header slot

--- frontend/src/components/PGTree.vue
+++ frontend/src/components/PGTree.vue
@@ -1,5 +1,7 @@
 <template>
-    <p>PGTree</p>
+    <div class="header">
+        <slot name="header"></slot>
+    </div>
     <el-tree :data="data" :props="defaultProps"></el-tree>
 </template>
 
@@ -18,4 +20,16 @@ const props = defineProps({
 
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.header {
+    width: 100%;
+    height: 42px;
+    font-weight: bold;
+    color: #fff;
+    background: rgb(45, 69, 153);
+    border-radius: 6px 6px 0 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
