1379f6b75ddb2cfb525b7fc6ae69fdffdcfeaecd
Gzx1999 <guozhengxin@kylinos.cn>
2023-11-21 15:12:33 +0800

    add header breadcrumb and user info

---
+++ frontend/src/views/Home/components/BreadCrumb.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item to="/overview">
+        <em class="el-icon-s-home"></em>
+        <span class="el-dropdown-link">
+          首页
+        </span>
+      </el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
+
+</template>
+  
+<script setup lang="ts">
+
+</script>
+  
+<style lang="scss" scoped></style>
