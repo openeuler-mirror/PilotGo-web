686614d11481fa76c8bed78a5ec1e11a59532d75
Gzx1999 <guozhengxin@kylinos.cn>
2023-11-28 11:01:49 +0800

    add machine service pane

---
+++ frontend/src/views/Cluster/MachineDetail/Service.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="content">
+        <div class="services">
+            <el-autocomplete style="width:50%" class="inline-input" v-model="serviceName"
+                placeholder="请输入服务名称"></el-autocomplete>
+            <el-button plain type="primary">搜索</el-button>
+            <el-button plain type="primary">启动</el-button>
+            <el-button plain type="primary">停止</el-button>
+            <el-button plain type="primary">重启</el-button>
+        </div>
+        <div class="info">
+            <div class="detail" v-if="display">
+                <p class="title">服务详情：</p>
+                <el-descriptions :column="2" size="medium" border>
+                    <el-descriptions-item label="服务名"> Name </el-descriptions-item>
+                    <el-descriptions-item label="状态"> Active </el-descriptions-item>
+                    <el-descriptions-item label="模块是否加载"> LOAD </el-descriptions-item>
+                    <el-descriptions-item label="SUB"> SUB </el-descriptions-item>
+                </el-descriptions>
+            </div>
+            <div class="result" v-else>
+                <p class="title">执行结果：</p>
+                <el-descriptions :column="2" size="medium" border>
+                    <el-descriptions-item label="软件包名">serviceName </el-descriptions-item>
+                    <el-descriptions-item label="执行动作">action </el-descriptions-item>
+                    <el-descriptions-item label="结果">
+                        {{ result + ":" }}
+                        <p class="progress" v-show="result != ''">
+                            <span>100%</span>
+                        </p>
+                    </el-descriptions-item>
+                </el-descriptions>
+            </div>
+        </div>
+
+    </div>
+</template>
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const serviceName = ref("")
+
+const display = ref(true)
+const result = ref("")
+
+</script>
+<style lang="scss" scoped></style>
