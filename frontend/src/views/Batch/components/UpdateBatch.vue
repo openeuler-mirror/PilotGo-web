4aae1d60dc7ee02b32dc53d6427c5af43839b7da
Gzx1999 <guozhengxin@kylinos.cn>
2023-12-04 14:13:49 +0800

    add component UpdateBatch

---
+++ frontend/src/views/Batch/components/UpdateBatch.vue
@@ -0,0 +1,23 @@
+<template>
+    <el-form :model="formData" label-width="100px">
+        <el-form-item label="批次名称" prop="name">
+            <el-input class="ipInput" type="text" v-model="formData.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="备注" prop="description">
+            <el-input class="ipInput" type="text" v-model="formData.description" autocomplete="off"></el-input>
+        </el-form-item>
+    </el-form>
+    <div>
+        <el-button>取 消</el-button>
+        <el-button type="primary">确 定</el-button>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const formData = ref<any>({})
+
+</script>
+
+<style lang="scss" scoped></style>
