1b61308ce573cc5c49932ee44bb9371aa9112532
Gzx1999 <guozhengxin@kylinos.cn>
2023-12-05 17:38:31 +0800

    add addUser and get all roles api

--- frontend/src/request/user.ts
+++ frontend/src/request/user.ts
@@ -2,35 +2,44 @@ import request from './request';
 
 // 用户登录
 export function loginByEmail(data: any) {
-  return request({
-    url: '/user/login',
-    method: 'post',
-    data,
-  });
+    return request({
+        url: '/user/login',
+        method: 'post',
+        data,
+    });
 }
 
 // 用户登出
 export function logout() {
-  return request({
-    url: '/user/logout',
-    method: 'get',
-  })
+    return request({
+        url: '/user/logout',
+        method: 'get',
+    })
 }
 
 // 获取全部用户信息
 export function getUsers(data: any) {
-  return request({
-    url: '/user/searchAll',
-    method: 'get',
-    params: data,
-  });
+    return request({
+        url: '/user/searchAll',
+        method: 'get',
+        params: data,
+    });
 }
 
 // 按邮箱查找用户
 export function searchUser(data: any) {
-  return request({
-    url: '/user/userSearch',
-    method: 'post',
-    data,
-  });
+    return request({
+        url: '/user/userSearch',
+        method: 'post',
+        data,
+    });
+}
+
+// 添加用户
+export function addUser(data: any) {
+    return request({
+        url: '/user/register',
+        method: 'post',
+        data
+    })
 }
