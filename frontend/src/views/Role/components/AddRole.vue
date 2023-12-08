3a9e30e411c968ac8fe1c7d47bb3e81497367be2
Gzx1999 <guozhengxin@kylinos.cn>
2023-12-08 16:09:20 +0800

    add component AddRole and UpdateRole

---
+++ frontend/src/views/Role/components/AddRole.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <p>add role</p>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped></style>
---
+++ frontend/src/views/Role/components/UpdateRole.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <p>update role</p>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped></style>
