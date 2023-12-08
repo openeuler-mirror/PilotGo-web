befbd0b7ddfb1281d18f17a641f1e5cd1cae0960
Gzx1999 <guozhengxin@kylinos.cn>
2023-12-08 15:13:57 +0800

    add role detail component

---
+++ frontend/src/views/Role/components/RoleDetail.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <el-tree ref="tree" :data="authData" :show-checkbox="showEdit" node-key="menuName" :check-strictly="strictly"
+            :default-expanded-keys="[2, 3]" :default-checked-keys="checkedKeys">
+        </el-tree>
+
+        <div class="footer" v-if="showEdit">
+            <el-button>取 消</el-button>
+            <el-button type="primary">确 定</el-button>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue';
+
+import { authData } from "./authData";
+
+const props = defineProps({
+    showEdit: {
+        type: Boolean,
+        default: true,
+    }
+})
+
+onMounted(() => {
+
+})
+
+const roleData = ref([])
+const checkedKeys = ref<string[]>([])
+const strictly = ref(true)
+
+</script>
+
+<style lang="scss" scoped>
+.footer {
+    text-align: right;
+}
+</style>
