663813e5a3af253826c0ea77093b62ec44598c6e
Gzx1999 <guozhengxin@kylinos.cn>
2023-12-11 15:19:16 +0800

    add form rule check

--- frontend/src/views/Role/components/AddRole.vue
+++ frontend/src/views/Role/components/AddRole.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <el-form :model="form" ref="form" label-width="100px">
+        <el-form :model="form" :rules="rules" ref="formRef" label-width="100px">
             <el-form-item label="角色名:" prop="rolename">
                 <el-input class="ipInput" type="text" size="medium" v-model="form.rolename" autocomplete="off"></el-input>
             </el-form-item>
@@ -19,6 +19,17 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
 
+const rules = {
+    rolename: [
+        {
+            required: true,
+            message: "请输入角色名",
+            trigger: "blur"
+        }
+    ],
+}
+
+const formRef = ref()
 const form = ref({
     rolename: "",
     description: ""
